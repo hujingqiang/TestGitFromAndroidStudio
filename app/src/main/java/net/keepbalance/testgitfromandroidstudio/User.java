a56da0fb2aa298ac97142853419cbf054fadb1f1
hujingqiang <hujingqiang@yahoo.com>
2017-11-21 20:33:28 +0800

    add user

---
+++ app/src/main/java/net/keepbalance/testgitfromandroidstudio/User.java
@@ -0,0 +1,15 @@
+package net.keepbalance.testgitfromandroidstudio;
+
+public class User {
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private String userName;
+
+}
